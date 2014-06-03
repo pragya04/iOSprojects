bde639f7cc80890df24b83dddf3a94993850d555
pherwani <pherwani@yahoo-inc.com>
2014-06-02 21:13:37 -0700

    Initial Commit

---
+++ tipcalculatorapp/TipCalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TipCalculatorAppDelegate.h
+//  tipcalculatorapp
+//
+//  Created by Pragya  Pherwani on 6/2/14.
+//  Copyright (c) 2014 Pragya  Pherwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipCalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculatorapp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculatorapp
+//
+//  Created by Pragya  Pherwani on 6/2/14.
+//  Copyright (c) 2014 Pragya  Pherwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TipCalculatorAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TipCalculatorAppDelegate class]));
+    }
+}
